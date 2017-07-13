99e7f09864e698745a78e7ebcab500b0da121013
Kathleen Weisensel <kweisensel@gmail.com>
2017-07-13 10:42:11 -0500

    test file added

---
+++ TreeFarm/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeFarm
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Tree
+    {
+        public double Height { get; set; } = 0;
+        public string Location { get; set; } = "";
+        public string Type { get; set; } = "";
+
+        public string Move (string newLocation)
+        {
+            if (newLocation != this.Location)
+            {
+                this.Location = newLocation;
+            }
+            return this.Location;
+        }
+
+        public double Grow (double growth)
+        {
+            if (growth > 0.0)
+            {
+                this.Height += growth;
+            }
+
+            return this.Height;
+        }
+    }
+}
